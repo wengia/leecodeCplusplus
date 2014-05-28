39e7069270e446bfbdc858a2a7cb17f9e2e28d45
wengia <wengia_mawenjia@hotmail.com>
2014-05-27 17:38:18 -0700

    merge two sorted list

--- normal_exercise/solution.h
+++ normal_exercise/solution.h
@@ -889,6 +889,28 @@ public:
 		while (j >= 0) A[k--] = B[j--];
 	}
 
+	// Merge Two Sorted Lists
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+		ListNode dummy(0);
+		ListNode *pre = &dummy;
+
+		while (l1 && l2) {
+			if (l1->val < l2->val) {
+				pre->next = l1;
+				l1 = l1->next;
+			}
+			else {
+				pre->next = l2;
+				l2 = l2->next;
+			}
+			pre = pre->next;
+		}
+		if (l1) pre->next = l1;
+		if (l2) pre->next = l2;
+
+		return dummy.next;
+	}
+
 private:
 	// Return the height of one branch
 	int treeHeight(TreeNode *tree) {
