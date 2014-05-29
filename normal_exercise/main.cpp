9bcf114a0672469ef4d3a96a3617f23fad116662
wengia <wengia_mawenjia@hotmail.com>
2014-05-29 16:31:46 -0700

    pascal triangle

--- normal_exercise/main.cpp
+++ normal_exercise/main.cpp
@@ -711,17 +711,14 @@ int main()
 
 	string a = "fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi";
 
-	vector<vector<string>> res;
-
-	/*res = solution.partition(a);
+	vector<vector<int>> res = solution.generate(5);
 
 	for (int i = 0; i < res.size(); i++) {
 		for (int j = 0; j < res[i].size(); j++)
 			cout << res[i][j] << '\t';
 		cout << endl;
-	}*/
+	}
 
-	solution.test();
 
 	cin>>stop;
 	return 0;
--- normal_exercise/solution.h
+++ normal_exercise/solution.h
@@ -1105,6 +1105,22 @@ public:
 		return dummy.next;
 	}
 
+	// Pascal's Triangle
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> res;
+		if (numRows == 0) return res;
+
+		for (int i = 1; i <= numRows; i++) {
+			vector<int> row(i, 1);
+			for (int j = 1; j < i - 1; j++) {
+				row[j] = res[i - 2][j - 1] + res[i - 2][j];
+			}
+			res.push_back(row);
+		}
+
+		return res;
+	}
+
 private:
 	// Return the height of one branch
 	int treeHeight(TreeNode *tree) {
