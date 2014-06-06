27fff57de2e9c42e6ad04ee362c32b116abb2d88
wengia <wengia_mawenjia@hotmail.com>
2014-06-06 09:48:40 -0700

    set matrix zero 1

--- normal_exercise/solution.h
+++ normal_exercise/solution.h
@@ -1442,6 +1442,28 @@ public:
 		return false;
 	}
 
+	// Set Matrix Zeroes
+	void setZeroes(vector<vector<int> > &matrix) {
+		if (matrix.size() == 0 || matrix[0].size() == 0) return;
+		int m = matrix.size(), n = matrix[0].size();
+		vector<int> row, col;
+
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (matrix[i][j] == 0) {
+					row.push_back(i);
+					col.push_back(j);
+				}
+
+		for (int i = 0; i < row.size(); i++)
+			for (int j = 0; j < n; j++)
+				matrix[row[i]][j] = 0;
+
+		for (int j = 0; j < col.size(); j++)
+			for (int i = 0; i < m; i++)
+				matrix[i][col[j]] = 0;
+	}
+
 private:
 	// Return the height of one branch
 	int treeHeight(TreeNode *tree) {
