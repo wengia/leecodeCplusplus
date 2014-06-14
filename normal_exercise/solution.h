e80b2e78fa3b8cb07a094f8d4d384edf42276ef2
wengia <wengia_mawenjia@hotmail.com>
2014-06-14 16:31:39 -0700

    unique path II

--- normal_exercise/solution.h
+++ normal_exercise/solution.h
@@ -1823,6 +1823,28 @@ public:
 		return res;
 	}
 
+	// Unique Path II
+	int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+		if (obstacleGrid.empty() || obstacleGrid[0].empty()) return 0;
+		int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+		int **dp = new int*[m + 1];
+
+		for (int i = 0; i <= m; i++) {
+			dp[i] = new int[n + 1];
+			dp[i][0] = 0;
+		}
+		for (int j = 1; j <= n; j++)
+			dp[0][j] = 0;
+
+		dp[0][1] = 1;
+		for (int i = 1; i <= m; i++)
+			for (int j = 1; j <= n; j++) {
+				dp[i][j] = obstacleGrid[i - 1][j - 1] == 1 ? 0 : (dp[i - 1][j] + dp[i][j - 1]);
+			}
+
+		return dp[m][n];
+	}
+
 private:
 	// Return the height of one branch
 	int treeHeight(TreeNode *tree) {
